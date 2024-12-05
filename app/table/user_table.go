0c824f43a70fbb8edfacef3aa2ce9dff43b3d001
srz-vishnu <vishnuk@smartrabbitz.com>
2024-12-05 17:56:49 +0530

    crud app- modifyed crud app using gorm-db and also introduced repo layer

---
+++ app/table/user_table.go
@@ -0,0 +1,17 @@
+package table
+
+// import (
+// 	"time"
+
+// 	"github.com/google/uuid"
+// )
+
+// type UserData struct {
+// 	ID               uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+// 	CreatedAt        time.Time `gorm:"autoCreateTime"`
+// 	UpdatedAt        time.Time `gorm:"autoUpdateTime"`
+// 	AssetReportId    string
+// 	AssetReportToken string
+// 	RequestId        string
+// 	CreditDecisionId uuid.UUID
+// }
--- main.go
+++ main.go
@@ -1,28 +1,45 @@
 package main
 
 import (
-	"database/sql"
+	"e-book/app/repo"
+	gormdb "e-book/pkg/gorm_db"
 	"fmt"
 	"log"
-	"time"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	db := ConnectDb()
-	defer db.Close()
+	db, err := gormdb.ConnectDb()
 
-	// Insert a new user
-	userID, err := CreatUser(db, "sherin@gmail.com", "sherin", "password", "text")
-	if err != nil {
-		log.Fatal("Error creating user:", err)
+	repoConn := repo.User{
+		Mail:     "dj@gmail.com",
+		Username: "Dj",
+		Password: "password1",
+		Salt:     "random",
 	}
 
-	fmt.Printf("New book created with ID: %d\n", userID)
+	// Create a new user
+	userIdentity, err := repo.CreateUser(db, repoConn.Mail, repoConn.Username, repoConn.Password, repoConn.Salt)
+	if err != nil {
+		log.Fatalf("Error creating user: %v", err)
+	}
+
+	// Print the created user's ID
+	fmt.Printf("User created with ID: %d\n", userIdentity)
+
+	// Get one User
+	idOfUser := int64(3)
+	usernamee, mail, err := repo.GetOneUser(db, idOfUser)
+	if err != nil {
+		log.Fatalf("Error retrieving user: %v", err)
+	}
+
+	// Print the result
+	fmt.Printf("User found with id: %d\n Username: %s\n Mail: %s\n", idOfUser, usernamee, mail)
 
 	// // Get all users
-	usernames, mails, err := GetAllUsers(db)
+	usernames, mails, err := repo.GetAllUsers(db)
 	if err != nil {
 		log.Fatalf("Error retrieving users: %v", err)
 	}
@@ -32,137 +49,12 @@ func main() {
 		fmt.Printf("Username: %s, Mail: %s\n", usernames[i], mails[i])
 	}
 
-	// Get one User
-	userId := int64(3)
-	username, mail, err := GetOneUser(db, userId)
-	if err != nil {
-		log.Fatalf("Error retrieving user: %v", err)
-	}
-
-	// Print the result
-	fmt.Printf("User found with id: %d\n Username: %s\n Mail: %s\n", userId, username, mail)
-
 	// Deleting an user with id given
-	err = DeleteUser(db, 3) // Delete the user with ID 5
+	err = repo.DeleteUser(db, 10) // Delete the user with ID
 	if err != nil {
 		log.Fatalf("Error deleting user: %v", err)
 	} else {
-		fmt.Println("User with id 3 deleted successfully")
+		fmt.Println("User deleted successfully")
 	}
 
-}
-
-func ConnectDb() *sql.DB {
-	// PostgreSQL connection string
-	connStr := "user=postgres password=1234 dbname=CrudApp sslmode=disable"
-
-	var err error
-
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatalf("Failed to open a DB connection: %v\n", err)
-	}
-
-	// Test the connection
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("Failed to connect to the database: %v\n", err)
-	}
-
-	fmt.Println("Successfully connected to the database!")
-
-	return db
-}
-
-type User struct {
-	ID       int64  `json:"id"`
-	Mail     string `json:"mail"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Salt     string `json:"salt"`
-}
-
-func CreatUser(db *sql.DB, mail string, username string, password string, salt string) (int64, error) {
-	query := `
-	INSERT INTO users (mail, username, password, salt)
-	VALUES ($1, $2, $3, $4) RETURNING id`
-
-	var identity int64
-	err := db.QueryRow(query, mail, username, password, salt).Scan(&identity)
-	if err != nil {
-		return 0, err
-	}
-
-	return identity, nil
-}
-
-func GetOneUser(db *sql.DB, userId int64) (string, string, error) {
-	query := `SELECT mail, username FROM users WHERE id = $1`
-
-	// variable to store usernames and mails
-	var username, mail string
-
-	// Use QueryRow since you're expecting a single row result
-	//err = db.QueryRow(query, userId).Scan(&mail, &username)
-	err := db.QueryRow(query, userId).Scan(&mail, &username)
-
-	if err != nil {
-		return "", "", err
-	}
-
-	return username, mail, nil
-}
-
-func GetAllUsers(db *sql.DB) ([]string, []string, error) {
-	query := `SELECT mail, username FROM users`
-
-	// Execute the query
-	rows, err := db.Query(query)
-	if err != nil {
-		return nil, nil, err
-	}
-	defer rows.Close()
-
-	// Slices to store usernames and mails
-	var userNames []string
-	var mails []string
-
-	// Iterate over rows
-	for rows.Next() {
-		var mail, username string
-		// Scan the values into mail and username variables
-		err := rows.Scan(&mail, &username)
-		if err != nil {
-			return nil, nil, err
-		}
-
-		// Append the retrieved values to the slices
-		mails = append(mails, mail)
-		userNames = append(userNames, username)
-	}
-
-	return userNames, mails, nil
-}
-
-func DeleteUser(db *sql.DB, userID int64) error {
-	// SQL query to delete a user by ID
-	query := `UPDATE users SET is_deleted=$1, deleted_at=$2 WHERE id=$3`
-
-	// Execute the query
-	result, err := db.Exec(query, true, time.Now().UTC(), userID)
-	if err != nil {
-		return err
-	}
-
-	// Check if any rows affected.
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return err
-	}
-
-	if rowsAffected == 0 {
-		return fmt.Errorf("no user found with ID %d", userID)
-	}
-
-	return nil
 }
